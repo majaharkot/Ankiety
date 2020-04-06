5f1556ebbbfcae0aa25c4d63ae4985e96616875a
majaharkot <harkot.majka@gmail.com>
2020-04-06 16:28:30 +0200

    utworzenie projektu - poprawka

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ankiety.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ankiety.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
