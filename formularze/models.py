effabcb582779720e5490527c9d359a258cfd477
Bananek96 <Banasiak_Jakub@wp.pl>
2020-04-06 21:00:03 +0200

    modele formularzy

--- formularze/models.py
+++ formularze/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Pytanie(models.Model):
+    pytanie = models.CharField(verbose_name='pytanie', max_length=100)
+
+    def __str__(self):
+        return self.pytanie
+
+    class Meta:
+        verbose_name_plural = 'pytania'
