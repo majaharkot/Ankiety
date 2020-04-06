ade4cf5b8402e85cf2e66a245cff9781f54bab2d
majaharkot <harkot.majka@gmail.com>
2020-04-06 17:55:38 +0200

    logowanie do serwisu - part 2

--- Ankiety/settings.py
+++ Ankiety/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-pl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -121,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates'),]
+
+LOGIN_URL='/users/loguj/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- users/views.py
+++ users/views.py
@@ -1,15 +1,49 @@
-from django.shortcuts import render
-from studenci.forms import UserLoginForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from users.forms import UserLoginForm
 
 def index(request):
-    return render(request,'users/index.html')
+    return render(request, 'users/index.html')
 
-def loguj_user(request):
-    """Widok wyświetlający formularz logowania"""
+def rejestruj(request):
     if request.method == 'POST':
-        pass
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Utworzono konto! Możesz się zalogować!")
+            return redirect(reverse('users:index'))
+    else:
+        form = UserCreationForm()
+
+    kontekst = {'form': form}
+    return render(request, 'users/rejestruj.html', kontekst)
+
+
+def loguj(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            nazwa = form.cleaned_data['login']
+            haslo = form.cleaned_data['haslo']
+            user = authenticate(request, username=nazwa, password=haslo)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Zostałeś zalogowany!")
+                return redirect(reverse('users:index'))
+            else:
+                messages.error(request, "Błędny login lub hasło!")
     else:
         form = UserLoginForm()
 
     kontekst = {'form': form}
-    return render(request, 'users/login.html', kontekst)
+    return render(request, 'users/loguj.html', kontekst)
+
+
+def wyloguj(request):
+    logout(request)
+    messages.info(request, "Zostałeś wylogowany!")
+    return redirect(reverse('users:index'))
